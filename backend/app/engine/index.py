9be7d5e5ac5b49b87b5efc9fe859f03624ae33d0
TwingzChenou <quentin-forget@hotmail.fr>
2026-02-09 14:25:05 +0100

    Update Data

--- backend/app/engine/index.py
+++ backend/app/engine/index.py
@@ -36,6 +36,7 @@ def setup_storage_context(pc_index):
 )
 
 #setup embeddings
+def setup_embeddings():
     return GeminiEmbedding(
         model_name="models/gemini-embedding-001",
         api_key=os.getenv("GOOGLE_API_KEY")
